f0fd2a80c031db4c0eda4b7fe753682ab60f4a9c
Leulassaminew <leulassaminew@gmail.com>
2023-10-27 17:02:52 +0300

    update

---
+++ finetune.py
@@ -0,0 +1,346 @@
+import os
+from random import randrange
+from functools import partial
+import torch
+from datasets import load_dataset
+from transformers import (AutoModelForCausalLM,
+                          AutoTokenizer,
+                          BitsAndBytesConfig,
+                          HfArgumentParser,
+                          Trainer,
+                          TrainingArguments,
+                          DataCollatorForLanguageModeling,
+                          EarlyStoppingCallback,
+                          pipeline,
+                          logging,
+                          set_seed)
+
+import bitsandbytes as bnb
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, PeftModel, AutoPeftModelForCausalLM
+from trl import SFTTrainer
+
+
+model_name = "meta-llama/Llama-2-13b-chat-hf"
+
+load_in_4bit = True
+bnb_4bit_use_double_quant = True
+bnb_4bit_quant_type = "nf4"
+bnb_4bit_compute_dtype = torch.bfloat16
+lora_r = 16
+lora_alpha = 16
+lora_dropout = 0.1
+bias = "none"
+task_type = "SEQ_CLS"
+output_dir = "outputs_squad"
+per_device_train_batch_size = 1
+gradient_accumulation_steps = 4
+learning_rate = 0.00005
+optim = "adafactor"
+max_steps = 60
+warmup_steps = 5
+fp16 = True
+logging_steps = 25
+
+
+def create_bnb_config(load_in_4bit, bnb_4bit_use_double_quant, bnb_4bit_quant_type, bnb_4bit_compute_dtype):
+    """
+    Configures model quantization method using bitsandbytes to speed up training and inference
+
+    :param load_in_4bit: Load model in 4-bit precision mode
+    :param bnb_4bit_use_double_quant: Nested quantization for 4-bit model
+    :param bnb_4bit_quant_type: Quantization data type for 4-bit model
+    :param bnb_4bit_compute_dtype: Computation data type for 4-bit model
+    """
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit = load_in_4bit,
+        bnb_4bit_use_double_quant = bnb_4bit_use_double_quant,
+        bnb_4bit_quant_type = bnb_4bit_quant_type,
+        bnb_4bit_compute_dtype = bnb_4bit_compute_dtype,
+    )
+
+    return bnb_config
+
+def load_model(model_name, bnb_config):
+    """
+    Loads model and model tokenizer
+
+    :param model_name: Hugging Face model name
+    :param bnb_config: Bitsandbytes configuration
+    """
+
+
+    # Load model
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        trust_remote_code=True, 
+        quantization_config = bnb_config,
+        device_map = "auto", # dispatch the model efficiently on the available resources
+        cache_dir="./models",
+    )
+
+    # Load model tokenizer with the user authentication token
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token = True,    trust_remote_code=True,
+    cache_dir="./models",)
+
+    # Set padding token as EOS token
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
+
+
+
+bnb_config = create_bnb_config(load_in_4bit, bnb_4bit_use_double_quant, bnb_4bit_quant_type, bnb_4bit_compute_dtype)
+model, tokenizer = load_model(model_name, bnb_config)
+
+dataset = load_dataset("Leul78/total")
+
+def create_prompt_formats(sample):
+    """
+    Creates a formatted prompt template for a prompt in the instruction dataset
+
+    :param sample: Prompt or sample from the instruction dataset
+    """
+
+    # Initialize static strings for the prompt template
+    INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+    INSTRUCTION_KEY = "### Instruction:"
+    INPUT_KEY = "Input:"
+    RESPONSE_KEY = "### Response:"
+    END_KEY = "### End"
+    instruct = "'Categorize the text based on the sales technique used in it from one of these categories only and offer no explanation:\n\nBUILDING RAPPORT\nNEEDS ASSESMENT\nCREATING URGENCY\nSOCIAL PROOF\nOVERCOMING OBJECTION\nCROSS SELLING OR UPSELLING\nVALUE BASED SELLING\nNONE\n\n"
+
+    # Combine a prompt with the static strings
+    blurb = f"{INTRO_BLURB}"
+    instruction = f"{INSTRUCTION_KEY}\n{instruct}"
+    input_context = f"{INPUT_KEY}\n{sample['text']}" if sample["text"] else None
+    response = f"{RESPONSE_KEY}\n{sample['category']}"
+    end = f"{END_KEY}"
+
+    # Create a list of prompt template elements
+    parts = [part for part in [blurb, instruction, input_context, response, end] if part]
+
+    # Join prompt template elements into a single string to create the prompt template
+    formatted_prompt = "\n\n".join(parts)
+
+    # Store the formatted prompt template in a new key "text"
+    sample["newtext"] = formatted_prompt
+
+    return sample
+
+def get_max_length(model):
+    """
+    Extracts maximum token length from the model configuration
+
+    :param model: Hugging Face model
+    """
+
+    # Pull model configuration
+    conf = model.config
+    # Initialize a "max_length" variable to store maximum sequence length as null
+    max_length = None
+    # Find maximum sequence length in the model configuration and save it in "max_length" if found
+    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
+        max_length = getattr(model.config, length_setting, None)
+        if max_length:
+            print(f"Found max lenth: {max_length}")
+            break
+    # Set "max_length" to 1024 (default value) if maximum sequence length is not found in the model configuration
+    if not max_length:
+        max_length = 1024
+        print(f"Using default max length: {max_length}")
+    return max_length
+
+def preprocess_batch(batch, tokenizer, max_length):
+    """
+    Tokenizes dataset batch
+
+    :param batch: Dataset batch
+    :param tokenizer: Model tokenizer
+    :param max_length: Maximum number of tokens to emit from the tokenizer
+    """
+
+    return tokenizer(
+        batch["newtext"],
+        max_length = max_length,
+        truncation = True,
+    )
+
+
+def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int, seed, dataset: str):
+    """
+    Tokenizes dataset for fine-tuning
+
+    :param tokenizer (AutoTokenizer): Model tokenizer
+    :param max_length (int): Maximum number of tokens to emit from the tokenizer
+    :param seed: Random seed for reproducibility
+    :param dataset (str): Instruction dataset
+    """
+
+    # Add prompt to each sample
+    print("Preprocessing dataset...")
+    dataset = dataset.map(create_prompt_formats)
+
+    # Apply preprocessing to each batch of the dataset & and remove "instruction", "input", "output", and "text" fields
+    _preprocessing_function = partial(preprocess_batch, max_length = max_length, tokenizer = tokenizer)
+    dataset = dataset.map(
+        _preprocessing_function,
+        batched = True,
+        remove_columns = ["instruction", "text", "category", "newtext"],
+    )
+
+    # Filter out samples that have "input_ids" exceeding "max_length"
+    dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+
+    # Shuffle dataset
+    dataset = dataset.shuffle(seed = seed)
+
+    return dataset
+
+
+# Random seed
+seed = 33
+
+max_length = get_max_length(model)
+preprocessed_dataset = preprocess_dataset(tokenizer, max_length, seed, dataset)
+
+def create_peft_config(r, lora_alpha, target_modules, lora_dropout, bias, task_type):
+    """
+    Creates Parameter-Efficient Fine-Tuning configuration for the model
+
+    :param r: LoRA attention dimension
+    :param lora_alpha: Alpha parameter for LoRA scaling
+    :param modules: Names of the modules to apply LoRA to
+    :param lora_dropout: Dropout Probability for LoRA layers
+    :param bias: Specifies if the bias parameters should be trained
+    """
+    config = LoraConfig(
+        r = r,
+        lora_alpha = lora_alpha,
+        target_modules = target_modules,
+        lora_dropout = lora_dropout,
+        bias = bias,
+        task_type = task_type,
+    )
+
+    return config
+
+def find_all_linear_names(model):
+    """
+    Find modules to apply LoRA to.
+
+    :param model: PEFT model
+    """
+
+    cls = bnb.nn.Linear4bit
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if 'lm_head' in lora_module_names:
+        lora_module_names.remove('lm_head')
+    print(f"LoRA module names: {list(lora_module_names)}")
+    return list(lora_module_names)
+
+
+def fine_tune(model,
+          tokenizer,
+          dataset,
+          lora_r,
+          lora_alpha,
+          lora_dropout,
+          bias,
+          task_type,
+          per_device_train_batch_size,
+          gradient_accumulation_steps,
+          warmup_steps,
+          max_steps,
+          learning_rate,
+          fp16,
+          logging_steps,
+          output_dir,
+          optim):
+    """
+    Prepares and fine-tune the pre-trained model.
+
+    :param model: Pre-trained Hugging Face model
+    :param tokenizer: Model tokenizer
+    :param dataset: Preprocessed training dataset
+    """
+
+    # Enable gradient checkpointing to reduce memory usage during fine-tuning
+    model.gradient_checkpointing_enable()
+
+    # Prepare the model for training
+    model = prepare_model_for_kbit_training(model)
+
+    # Get LoRA module names
+    target_modules = find_all_linear_names(model)
+
+    # Create PEFT configuration for these modules and wrap the model to PEFT
+    peft_config = create_peft_config(lora_r, lora_alpha, target_modules, lora_dropout, bias, task_type)
+    model = get_peft_model(model, peft_config)
+
+    # Training parameters
+    trainer = Trainer(
+        model = model,
+        train_dataset = dataset,
+        args = TrainingArguments(
+            per_device_train_batch_size = per_device_train_batch_size,
+            gradient_accumulation_steps = gradient_accumulation_steps,
+            warmup_steps = warmup_steps,
+            max_steps = max_steps,
+            learning_rate = learning_rate,
+            fp16 = fp16,
+            logging_steps = logging_steps,
+            output_dir = output_dir,
+            optim = optim,
+        ),
+        data_collator = DataCollatorForLanguageModeling(tokenizer, mlm = False)
+    )
+
+    model.config.use_cache = False
+
+    do_train = True
+
+    # Launch training and log metrics
+    print("Training...")
+
+    if do_train:
+        train_result = trainer.train()
+        metrics = train_result.metrics
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+        print(metrics)
+
+    # Save model
+    print("Saving last checkpoint of the model...")
+    os.makedirs(output_dir, exist_ok = True)
+    trainer.model.save_pretrained(output_dir)
+
+    # Free memory for merging weights
+    del model
+    del trainer
+    torch.cuda.empty_cache()
+    
+
+    fine_tune(model,
+      tokenizer,
+      preprocessed_dataset,
+      lora_r,
+      lora_alpha,
+      lora_dropout,
+      bias,
+      task_type,
+      per_device_train_batch_size,
+      gradient_accumulation_steps,
+      warmup_steps,
+      max_steps,
+      learning_rate,
+      fp16,
+      logging_steps,
+      output_dir,
+      optim)
